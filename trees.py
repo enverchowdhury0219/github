89c4c53bb45bd3da06800eca58c6d629d7a066e5
enver chowdhury <enver.chowdhury@mail.utoronto.ca>
2025-08-26 17:52:55 -0500

    day 25

--- trees.py
+++ trees.py
@@ -36,3 +36,10 @@
 #         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
 # day 25
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
